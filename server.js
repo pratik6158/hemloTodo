8b7b87c8420e81417cd399111c8ddc36b60659e3
Pratik <amnshrma341@gmail.com>
2022-09-14 03:29:08 +0530

    added json

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const fs = require('fs')
+const { json } = require('body-parser')
 
 const run = express()
 run.use(express.urlencoded());
@@ -16,6 +17,31 @@ run.get('/idx',(req,res)=>{
 
 run.post('/todo',(req,res)=>{
     console.log(req.body.data)
+    var dta={
+        task:req.body.data
+    }
+    var data=JSON.stringify(dta)
+    const path="./data.json";
+    if(fs.existsSync(path)){
+        //file exist
+        try{
+            fs.appendFileSync("data.json",data)
+            fs.appendFileSync("data.json",',')
+            console.log("Write was sucessful")
+        }catch(err){
+            console.log("ERROE line 31")
+        }
+    }else{
+        //file does not exist
+        fs.writeFile('./data.json','['+data+',',err=>{
+            if(err){
+                console.log("There was some problem");
+                return;
+            }
+            console.log('data saved successfully')
+        })
+        // fs.appendFileSync("data.json",',')
+    }
     res.send("Got It")
 })
 run.listen(5000)
